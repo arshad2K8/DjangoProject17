5cf370282089bc74f9755ec323712b59353b46bd
arshad2k8 <arshad2k7@gmail.com>
2016-12-18 19:55:13 +0000

    testing

--- tango_with_django_project17/rango/views.py
+++ tango_with_django_project17/rango/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     #return HttpResponse("Rango Says Hello World")
-    context_dict = {'boldmessage':"I am bold from the context"}
+    context_dict = {'boldmessage':"I am bold arshad from the context"}
     return render(request, 'rango/index.html', context_dict)
 
