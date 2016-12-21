fd132081d7f6959006618ae49e74d646a37033f8
arshad2k8 <arshad2k7@gmail.com>
2016-12-21 00:02:56 +0000

    Category template added

--- tango_with_django_project17/rango/admin.py
+++ tango_with_django_project17/rango/admin.py
@@ -4,7 +4,11 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
+# Add in this class to customized the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 # Register your models here.
 
--- tango_with_django_project17/rango/views.py
+++ tango_with_django_project17/rango/views.py
@@ -15,3 +15,18 @@ def index(request):
 def about(request):
     return HttpResponse("About rango mmm lemme think")
 
+
+
+def category(request, cat_name_slug):
+    try:
+        context_dict = {}
+        category = Category.objects.get(slug=cat_name_slug)
+        context_dict['category'] = category
+        context_dict['category_name'] = category.name
+        context_dict['slug_category_name'] = cat_name_slug
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html',context_dict)
